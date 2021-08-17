9be792f60e351313a9fa814c23a1b33667ce6e4e
Riasad Rion <riasadrion@gmail.com>
2021-08-17 21:52:04 +0600

    final push

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const express = require('express');
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen
 app.listen(3000);
 
-
-app.get('/', function(req, res) {
-    res.send('<p>Home</p>');
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.get('/about', function(req, res) {
-    res.send('<p>About</p>');
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404');
 });
